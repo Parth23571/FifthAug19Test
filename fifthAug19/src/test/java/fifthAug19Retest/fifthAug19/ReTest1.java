f970a782456b807ea5f2b254dff89e0ae4bf68c4
parth235 <pawal.pankaj@gmail.com>
2019-08-05 17:00:30 +0530

    First Commit

---
+++ fifthAug19/src/test/java/fifthAug19Retest/fifthAug19/ReTest1.java
@@ -0,0 +1,24 @@
+package fifthAug19Retest.fifthAug19;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class ReTest1 {
+  @Test
+  public void f() {
+	  System.out.println("Hello TestNG");
+	  
+	  System.setProperty("webdriver.chrome.driver","C:\\Browers\\chromedriver.exe");
+
+	  //String strPath="https://www.google.com";
+	  String strPath="file:///C:/Satish/JMP/IngeniousLogin.html";
+	  WebDriver driver=new ChromeDriver();
+	  driver.get(strPath);
+	  driver.manage().window().maximize();
+	  driver.findElement(By.name("userid")).sendKeys("admin");
+	  driver.findElement(By.name("pwd")).sendKeys("password");
+	  driver.findElement(By.xpath("//input[@value='Login']")).click();
+  }
+}
